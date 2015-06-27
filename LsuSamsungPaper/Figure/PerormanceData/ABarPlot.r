b8f948de5454e6ebf2735523d9764597dd333d23
arghyakusumdas <arghyakusumdas2266@gmail.com>
2015-06-27 18:39:59 -0500

    review1

--- LsuSamsungPaper/Figure/PerormanceData/ABarPlot.r
+++ LsuSamsungPaper/Figure/PerormanceData/ABarPlot.r
@@ -9,11 +9,10 @@ counts <- table(data$GraphConstruction, data$GraphSimplification, data$Scaffoldi
 #print(typeof(data$GraphConstruction))
 
 #Plot for perf and perf/$
-#barplot(as.matrix(data), main="", ylim=c(0,4), cex.names=0.9, xlab="Assembly phases",ylab="Execution time (s): Normalized to SuperMikeII", col=c("darkblue","red", "black", "yellow", "green", "orange", "grey"), beside=TRUE)
-#legend("topleft", legend = rownames(data), fill = c("darkblue","red", "black", "yellow", "green", "orange", "grey"), ncol = 1, cex = 0.75)
+#barplot(as.matrix(data), main="", ylim=c(0, 4), cex.names=0.9, xlab="Assembly Stages",ylab="Performance/$: Normalized to SuperMikeII", col=c("darkblue","blue", "darkred", "red", "darkgreen", "green", "black", "yellow", "grey"), beside=TRUE)
+#legend("topleft", legend = rownames(data), fill = c("darkblue","blue", "darkred", "red", "darkgreen", "green", "black", "yellow", "grey"), ncol = 1, cex = 0.6)
 
 #plot for network
-barplot(as.matrix(data), main="", ylim=c(0,1.5), cex.names=0.9, xlab="Assembly stages",ylab="Execution time (s): Normalized to SuperMikeII", col=c("darkblue","pink"), beside=TRUE)
-legend("topleft", legend = rownames(data), fill = c("darkblue","pink"), ncol = 1, cex = 0.75)
-
+barplot(as.matrix(data), main="", ylim=c(0,1.5), cex.names=0.9, xlab="",ylab="Execution time (s): normalized to SuperMikeII-baseline", col=c("black","pink"), beside=TRUE)
+legend("topright", legend = rownames(data), fill = c("black","pink"), ncol = 1, cex = 0.75)
 box()
