3a247b70eee9b103421ae5a26d93d44d069c7645
arghyakusumdas <arghyakusumdas2266@gmail.com>
2015-09-09 16:12:09 -0500

    CR1

--- LsuSamsungPaper/Figure/SystemData/AreadCSVandPlot.R
+++ LsuSamsungPaper/Figure/SystemData/AreadCSVandPlot.R
@@ -1,21 +1,21 @@
 #Where the file is saved
-pdf("/home/hadoop/rough/MyGITProjects/PGA_EvaluationArchitecturePaper/Evaluation-architectures-for-big-data/LsuSamsungPaper/Figure/SystemData/Plots/ECCPUHDD.pdf", family="Times")
+pdf("/home/hadoop/rough/MyGITProjects/PGA_EvaluationArchitecturePaper/Evaluation-architectures-for-big-data/LsuSamsungPaper/Figure/SystemData/Plots/ECHddSsdHdfsWrIops.pdf", family="Times")
 
 #Read data from CSV
-data <- read.csv(file="EC_1HDD_CPUCopy.csv",head=FALSE,sep=",")
+data <- read.csv(file="EC_1HDD_HDFSrwps.csv",head=FALSE,sep=",")
 
-x <- 1:length(data$V1)
+x <- 1:length(data$V9)
 
 lt <- c(1,2)
 
-plot(x, data$V1, type='l', col='blue', lwd=2, lty=lt[1], ann=FALSE, cex.axis=1.5, ylim=c(0,70))
-lines(x, data$V2, col='black', lwd=2, lty=lt[2])
+plot(x, data$V19, type='l', col='blue', lwd=2, lty=lt[1], ann=FALSE, cex.axis=1.3)
+lines(x, data$V9, col='black', lwd=2, lty=lt[2])
 
-title(xlab="Time (minutes)", cex.lab=2)
-title(ylab="%", cex.lab=2)
+title(xlab="Time (minutes)", cex.lab=1.3)
+title(ylab="HDFS write throughput (Bytes/s)", cex.lab=1.3)
 #title(main="IOPS Over Time")
 
-legend("topright", c("CPU utilization", "I/O wait"), col=c("blue","black"), lwd=c(2,2), lty=lt, cex = 1.5)
+legend("topright", c("1SSD/node", "1HDD/node"), col=c("blue","black"), lwd=c(2,2), lty=lt, cex = 1.5)
 
 #print(data$V5)
 dev.off()
